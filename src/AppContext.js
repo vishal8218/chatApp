76759cd5d4571fd1c56e5400418414cfcec1a04e
vishal8218 <vk368065@gmail.com>
2026-08-10 17:14:00 +0530

    feat: initialize AppContext and AppProvider for global base URL state management

--- src/AppContext.js
+++ src/AppContext.js
@@ -4,7 +4,7 @@ import { createContext, useContext, useState } from "react";
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const [baseUrl, setBaseUrl] = useState("https://chatwebapp1-0-1.onrender.comdcv/");
+  const [baseUrl, setBaseUrl] = useState("https://chatwebapp1-0-1.onrender.com/");
 
   return (
     <AppContext.Provider value={{ baseUrl, setBaseUrl }}>
